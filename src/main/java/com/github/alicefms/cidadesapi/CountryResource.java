18e7a12f866ded04d7e7b88bca6348ecfe82ae7f
Alice <alicefms@hotmail.com>
2021-05-20 13:21:20 -0300

    paginacao ok

--- src/main/java/com/github/alicefms/cidadesapi/CountryResource.java
+++ src/main/java/com/github/alicefms/cidadesapi/CountryResource.java
@@ -2,6 +2,8 @@ package com.github.alicefms.cidadesapi;
 
 import com.github.alicefms.cidadesapi.countries.Country;
 import com.github.alicefms.cidadesapi.repository.CountryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +23,7 @@ public class CountryResource {
 
 
     @GetMapping
-    public List<Country> countries(){
-        return repository.findAll();
+    public Page<Country> countries(Pageable page){
+        return repository.findAll(page);
     }
 }
