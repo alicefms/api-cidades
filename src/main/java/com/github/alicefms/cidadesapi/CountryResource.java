7080ee9c2db1b570c5053a6066d66dc215007a8c
Alice <alicefms@hotmail.com>
2021-05-20 13:42:35 -0300

    buscando por id

--- src/main/java/com/github/alicefms/cidadesapi/CountryResource.java
+++ src/main/java/com/github/alicefms/cidadesapi/CountryResource.java
@@ -5,10 +5,12 @@ import com.github.alicefms.cidadesapi.repository.CountryRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -25,5 +27,11 @@ public class CountryResource {
     @GetMapping
     public Page<Country> countries(Pageable page){
         return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public Country getOne(@PathVariable Long id) {
+        Optional<Country> optional = repository.findById(id);
+        return optional.get();
     }
 }
