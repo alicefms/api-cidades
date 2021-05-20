38e5fb944b5efbf5dd48de133e859d70e6cdca94
Alice <alicefms@hotmail.com>
2021-05-20 16:20:00 -0300

    request de cidades retornando adequadamente

---
+++ src/main/java/com/github/alicefms/cidadesapi/cities/CityRepository.java
@@ -0,0 +1,6 @@
+package com.github.alicefms.cidadesapi.cities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+}
