bc24de3a045e5bbc9e530fd04458f85e197cf3ec
Alice <alicefms@hotmail.com>
2021-05-20 18:12:18 -0300

    aplicando services para calcular distancia

--- src/main/java/com/github/alicefms/cidadesapi/cities/CityRepository.java
+++ src/main/java/com/github/alicefms/cidadesapi/cities/CityRepository.java
@@ -3,4 +3,7 @@ package com.github.alicefms.cidadesapi.cities;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CityRepository extends JpaRepository<City, Long> {
+    Double distanceByPoints(Long city1, Long city2);
+
+    Double distanceByCube(double x, double y, double x1, double y1);
 }
